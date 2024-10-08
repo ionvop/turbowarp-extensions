de7246916633223de26c6d9b5fa73c3899040715
ionvop <ionvop@gmail.com>
2024-10-08 16:27:30 +0800

    Update postMessage.js

--- postMessage.js
+++ postMessage.js
@@ -13,9 +13,9 @@
             return {
                 id: "ionvoppostmessage",
                 name: "PM",
-                color1: "#001111",
+                color1: "#00aaaa",
                 color2: "#005555",
-                color3: "#00aaaa",
+                color3: "#001111",
                 docsURI: "https://github.com/ionvop/turbowarp-extensions",
                 blocks: [
                     {
